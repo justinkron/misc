90aa0c4dd7644142dfe8267842d62af889045daa
justinkron <justinkron@gmail.com>
2024-09-09 08:50:18 -0500

    Add files via upload

---
+++ get-tpm key.ps1
@@ -0,0 +1,13 @@
+﻿$VMHosts = get-vmhost | Sort-Object
+
+foreach ($VMHost in $VMHosts) {
+    $esxcli = Get-EsxCli -VMHost $VMHost
+    try {
+        $key = $esxcli.system.settings.encryption.recovery.list()
+        Write-Host "$VMHost;$($key.RecoveryID);$($key.Key)"
+    }
+
+    catch {
+        
+    }
+}
