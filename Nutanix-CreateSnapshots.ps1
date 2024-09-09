b9e7e642e057880eef74b4576401f72528768200
justinkron <justinkron@gmail.com>
2024-09-09 08:53:27 -0500

    Add files via upload

---
+++ Nutanix-CreateSnapshots.ps1
@@ -0,0 +1,34 @@
+﻿# load Nutanix PS
+& "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\powershell\import_modules\ImportModules.PS1"
+
+# Primary
+Connect-NutanixCluster -Server ip.ip.ip.ip -username admin -AcceptInvalidSSLCerts
+
+
+#$VMs = Get-Content C:\scripts\Round3.txt
+
+
+$VMs = Get-NTNXVM | where {($_.controllervm -eq $false) -and ($_.powerstate -eq "on") -and ($_.description -notmatch "no_updates") -and ($_.vmname -notmatch "mdm")}
+
+foreach ($vm in $VMs)
+{
+    # Get VM object
+    $nutVM = get-ntnxvm -SearchString $VM
+    
+    #snapshot info
+    $snapshotName = "before-updates"
+    $newSnapshot = New-NTNXObject -Name SnapshotSpecDTO
+    $newSnapshot.vmuuid = $nutvm.uuid
+    $newSnapshot.snapshotname = $snapshotName
+    New-NTNXSnapshot -SnapshotSpecs $newSnapshot
+
+    # Get existing snapshots
+    Get-NTNXSnapshot | Where-Object {$_.vmUuid -eq $nutvm.uuid}
+
+      
+}
+
+disConnect-NutanixCluster -Server ip.ip.ip.ip
+
+
+
---
+++ Nutanix-DeleteSnapshots.ps1
@@ -0,0 +1,50 @@
+﻿# load Nutanix PS
+& "C:\Program Files (x86)\Nutanix Inc\NutanixCmdlets\powershell\import_modules\ImportModules.PS1"
+
+# Primary
+#Connect-NutanixCluster -Server ip.ip.ip.ip -username admin -AcceptInvalidSSLCerts
+
+
+Connect-NutanixCluster -Server ip.ip.ip.ip -username admin -AcceptInvalidSSLCerts
+
+#$VMs = Get-Content C:\scripts\Round3.txt
+
+# review this line, test against other cluster
+$VMs = Get-NTNXVM | where {($_.controllervm -eq $false) -and ($_.powerstate -eq "on") -and ($_.description -notmatch "no_updates") -and ($_.vmname -notmatch "mdm")}
+
+foreach ($vm in $VMs)
+{
+    $vm.vmname
+    
+    # Get VM object
+    $nutVM = get-ntnxvm -SearchString $VM.vmName
+    
+    # Get existing snapshots
+    Get-NTNXSnapshot | Where-Object {$_.vmUuid -eq $nutvm.uuid} 
+    
+    #snapshot removal
+    $snapshotName = "before-updates"
+    $snapshots = Get-NTNXSnapshot | Where-Object {($_.vmUuid -eq $nutvm.uuid) -and ($_.snapshotname -eq $snapshotName)} 
+    Foreach ($snapshot in $snapshots)
+    {
+        Write-Verbose "Removing snapshot:$($snapshot | Select-Object snapshotName,uuid | Format-List | Out-String)" -Verbose
+        Remove-NTNXSnapshot -Uuid $snapshot.uuid
+        
+    }
+    
+    # Get existing snapshots
+    Get-NTNXSnapshot | Where-Object {$_.vmUuid -eq $nutvm.uuid}      
+}
+
+
+
+disConnect-NutanixCluster -Server ip.ip.ip.ip
+
+
+
+
+
+
+
+
+
